22b4e2992888025c2f0500675f3c65ffb008e26c
MirrorXu <xuxulee@163.com>
2017-11-27 18:02:07 +0800

    视频页完成

---
+++ src/js/videoData.js
@@ -0,0 +1,48 @@
+
+var viedosData = [
+	{
+		id:'001',
+		pubPath:'http://www.thorgene.org/videos/Sampledeliveryprocedure.mp4',
+		title:'索真医学样本送检流程',
+		keywords:'索真医学样本送检流程',
+		videoImg:'http://www.thorgene.org/videos/img/Sampledeliveryprocedure.png',
+		author:'Mirror',
+		play:0
+	},{
+
+		id:'002',
+		pubPath:'http://www.thorgene.org/videos/ReportinterpretationofBrillianceofLife.mp4',
+		title:'生命之光报告解读',
+		keywords:'生命之光报告解读',
+		videoImg:'http://www.thorgene.org/videos/img/ReportinterpretationofBrillianceofLife.png',
+		author:'Mirror',
+		play:0
+	},{
+
+		id:'003',
+		pubPath:'http://www.thorgene.org/videos/IntroductionofInheritedDiseaseandDrugMetabolismGeneDetection.mp4',
+		title:'遗传病及药物代谢基因检测产品介绍',
+		keywords:'遗传病及药物代谢基因检测产品介绍',
+		videoImg:'http://www.thorgene.org/videos/img/IntroductionofInheritedDiseaseandDrugMetabolismGeneDetection.png',
+		author:'Mirror',
+		play:0
+	},{
+
+		id:'004',
+		pubPath:'http://www.thorgene.org/videos/IntroductionofTumorGeneDetection.mp4',
+		title:'肿瘤基因检测产品介绍',
+		keywords:'肿瘤基因检测产品介绍',
+		videoImg:'http://www.thorgene.org/videos/img/IntroductionofTumorGeneDetection.png',
+		author:'Mirror',
+		play:0
+	},{
+
+		id:'005',
+		pubPath:'http://www.thorgene.org/videos/IntroductionofHealthyPeopleGeneDetection.mp4 ',
+		title:'大众健康基因检测产品介绍',
+		keywords:'大众健康基因检测产品介绍',
+		videoImg:'http://www.thorgene.org/videos/img/IntroductionofHealthyPeopleGeneDetection.png',
+		author:'Mirror',
+		play:0
+	}
+];
